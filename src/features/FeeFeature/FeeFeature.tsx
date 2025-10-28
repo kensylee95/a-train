42a12b7b2756b8fa57497790c69f0120f01dcc47
kensylee95 <kingsleyumenze@gmail.com>
2025-10-28 22:36:51 +0100

    updated code structure

--- src/features/FeeFeature/FeeFeature.tsx
+++ src/features/FeeFeature/FeeFeature.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { useState } from "react";
+import styles from "./FeeFeature.module.scss";
 
 export function FeeFeature({ fees }: { fees: FeesInterface }) {
   const [serviceFees, setServiceFees] = useState(fees);
 
-  return <div></div>;
+  return <div className={styles.container}>//</div>;
 }
