2b42eb929a5bc2c0cbe97be871c6194c99c592a5
kensylee95 <kingsleyumenze@gmail.com>
2025-10-28 21:57:30 +0100

    updated code structure

---
+++ src/features/FeeFeature/FeeFeature.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { useState } from "react"
+
+export function FeeFeature({fees}:{fees: FeesInterface}){
+    const [serviceFees, setServiceFees] = useState(fees)
+    
+    return (
+        <div></div>
+    )
+}
---
+++ src/features/FeeFeature/types.ts
@@ -0,0 +1,28 @@
+interface ServiceItem {
+  Service: string;
+  Fee: string;
+  Description: string;
+}
+
+interface CustomerServices {
+  'US Virtual Bank Account': ServiceItem[];
+  'NG Virtual Bank Account': ServiceItem[];
+  'Payout': ServiceItem[];
+  'Wallet to Wallet Transfer': ServiceItem[];
+  'Freedom Virtual Card': ServiceItem[];
+  'FX': ServiceItem[];
+}
+
+interface BusinessServices {
+  'US Virtual Bank Account': ServiceItem[];
+  'NG Virtual Bank Account': ServiceItem[];
+  'Business Collections': ServiceItem[];
+  'Business Payout': ServiceItem[];
+  'Wallet to Wallet Transfer': ServiceItem[];
+  'FX': ServiceItem[];
+}
+
+interface FeesInterface {
+  Customer: CustomerServices;
+  Business: BusinessServices;
+}
